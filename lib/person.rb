2a0130e67b956621bbecf34ee6c999e24a1513c2
github username <github email address>
2019-03-13 14:55:56 -0600

    instantiated 'Person' class with basic attributes; clean, happy, and get_paid methods

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,26 @@
-# your code goes here
+class Person
+
+attr_reader :name
+attr_accessor :bank_account, :happiness, :hygiene
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def clean?
+    @hygiene > 7
+  end
+
+  def happy?
+    @happiness > 7
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    return "all about the benjamins"
+  end
+
+end
